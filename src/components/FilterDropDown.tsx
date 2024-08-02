5e458ad2d08f7dc59c2f5a37e894da4e3d4df188
Cristina Secu <secu.cristina@yahoo.com>
2024-08-01 23:40:43 -0400

    implement Filter by Region select element

--- src/components/FilterDropDown.tsx
+++ src/components/FilterDropDown.tsx
@@ -1,8 +1,41 @@
+import Select, { SingleValue } from "react-select";
+import { useCountries } from "../hooks/useCountries";
 import { useState } from "react";
-interface FilterDropDownProps {}
 
-function FilterDropDown(props: FilterDropDownProps): React.JSX.Element {
-  return <p>Filter dropdown</p>;
+interface SelectedOptions {
+  value: string;
+  label: string;
+}
+
+const options: SelectedOptions[] = [
+  { value: "Americas", label: "Americas" },
+  { value: "Europe", label: "Europe" },
+  { value: "Asia", label: "Asia" },
+  { value: "Africa", label: "Africa" },
+  { value: "Oceania", label: "Oceania" },
+];
+
+function FilterDropDown(): React.JSX.Element {
+  const { countries } = useCountries();
+  const [selected, setSelected] = useState<string | null>(null);
+
+  function handleChange(selectedOption: SingleValue<SelectedOptions>) {
+    if (!selectedOption) return;
+    setSelected(selectedOption.label);
+  }
+
+  const selectedRegion = countries.filter((country) => {
+    return country.region === selected;
+  });
+
+  console.log(selectedRegion);
+  return (
+    <Select
+      options={options}
+      onChange={handleChange}
+      placeholder={"Filter by region"}
+    />
+  );
 }
 
 export default FilterDropDown;
