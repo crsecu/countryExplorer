82e76288468954ab5e6e06f553afc20d5a985376
Cristina Secu <secu.cristina@yahoo.com>
2024-09-26 13:33:40 -0400

    implement Theme Switcher using localStorage and CSS variables; useLocalStorage hook is used to persist theme selection across sessions

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,35 @@ import PageNotFound from "./pages/PageNotFound";
 import Header from "./components/Header/Header";
 import NavBar from "./components/NavBar/NavBar";
 import ThemeSwitcher from "./components/ThemeSwitcher/ThemeSwitcher";
+import useLocalStorage from "use-local-storage";
 
 function App(): React.JSX.Element {
+  const defaultDark: boolean = window.matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches;
+
+  const [theme, setTheme] = useLocalStorage<"light" | "dark">(
+    "theme",
+    defaultDark ? "dark" : "light"
+  );
+
   return (
-    <CountriesProvider>
-      <BrowserRouter>
-        <Header>
-          <NavBar />
-          <ThemeSwitcher />
-        </Header>
-        <Routes>
-          <Route index element={<HomePage />} />
-          {/* <Route path="country/:cca3/:countryName" element={<DetailPage />} /> */}
-          <Route path="country/:countryName/:cca3" element={<DetailPage />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </CountriesProvider>
+    <div data-theme={theme}>
+      <CountriesProvider>
+        <BrowserRouter>
+          <Header>
+            <NavBar />
+            <ThemeSwitcher setTheme={setTheme} theme={theme} />
+          </Header>
+          <Routes>
+            <Route index element={<HomePage />} />
+            {/* <Route path="country/:cca3/:countryName" element={<DetailPage />} /> */}
+            <Route path="country/:countryName/:cca3" element={<DetailPage />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </CountriesProvider>
+    </div>
   );
 }
 
--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,7 +1,19 @@
-interface ThemeSwitcherProps {}
+interface ThemeSwitcherProps {
+  theme: "light" | "dark";
+  setTheme: (theme: "light" | "dark") => void;
+}
 
-function ThemeSwitcher(props: ThemeSwitcherProps): React.JSX.Element {
-  return <p>Theme Switcher</p>;
+function ThemeSwitcher({
+  setTheme,
+  theme,
+}: ThemeSwitcherProps): React.JSX.Element {
+  function switchTheme() {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  }
+  console.log("current theme", theme);
+
+  return <button onClick={switchTheme}>Theme Switcher</button>;
 }
 
 export default ThemeSwitcher;
