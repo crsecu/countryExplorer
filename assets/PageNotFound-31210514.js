255f98c8c46023f125176ec44a63cb927b12ac66
Cristina Secu <secu.cristina@yahoo.com>
2024-10-26 10:28:26 -0400

    Updates

--- assets/PageNotFound-31210514.js
+++
@@ -1,1 +0,0 @@
-import{j as o,S as t}from"./index-730a90f1.js";import{B as n}from"./Button-a201c909.js";function r(){return o.jsx(o.Fragment,{children:o.jsx(t,{buttonComponent:n,buttonName:"Back to Home",suggestion:"Sorry, the page you are looking for doesn't exist. You can go back to the main page by clicking the button below.",children:"Page not found 😢"})})}export{r as default};
